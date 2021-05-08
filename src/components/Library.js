120e1c2fe58290750f647a78f5dace2c6bc54dfb
Terry Kim <terrykim025@gmail.com>
2021-05-08 17:57:39 +0900

    functionality - switch between songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,18 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            song={song}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            key={song.id}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong }) => {
+  const songSelectHandler = () => {
+    setCurrentSong(song);
+  };
+
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={songSelectHandler}>
       <img src={song.cover} alt={"album cover for" + song.name}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
