32cc6ce7b23826475d47f53f9e9db44dde038a48
Terry Kim <terrykim025@gmail.com>
2021-05-10 19:07:44 +0900

    Add pulse animation album cover when song is playing

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Song = ({ currentSong }) => {
+const Song = ({ currentSong, isPlaying }) => {
   return (
     <div className="song-container">
       <img
         src={currentSong.cover}
         alt={"album cover for" + currentSong.name}
+        className={isPlaying ? "pulse-animate" : ""}
       ></img>
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
